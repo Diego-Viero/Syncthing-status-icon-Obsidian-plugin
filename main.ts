120c6293fe0ab78b218e071d0b3b717e06b8b238
Diego-Viero <diego.viero@protonmail.com>
2024-12-07 19:21:46 +0100

    implement status icon

---
+++ main.ts
@@ -0,0 +1,50 @@
+import { SyncthingMonitor } from 'checkStatus';
+import { Plugin } from 'obsidian';
+
+const GREEN_CIRCLE = '🟢';
+const RED_CIRCLE = '🔴';
+
+interface SyncthingStatusSettings {
+	interval: number;	// Seconds		
+}
+
+const DEFAULT_SETTINGS: SyncthingStatusSettings = {
+	interval: 30	// Seconds
+}
+
+export default class SyncthingStatus extends Plugin {
+	settings: SyncthingStatusSettings;
+
+
+	async onload() {
+		await this.loadSettings();
+
+		const monitor = new SyncthingMonitor();
+
+		const statusBarItemEl = this.addStatusBarItem();
+
+		statusBarItemEl.setText('Loading...');
+
+		monitor.on('connected', () => {
+			statusBarItemEl.setText(GREEN_CIRCLE);
+			statusBarItemEl.setAttribute('title', 'Connected to Syncthing');
+		});
+
+		monitor.on('disconnected', () => {
+			statusBarItemEl.setText(RED_CIRCLE);
+			statusBarItemEl.setAttribute('title', 'Disconnected from Syncthing');
+		});
+
+		this.registerInterval(window.setInterval(() => monitor.checkStatus(), this.settings.interval * 1000));
+
+		monitor.checkStatus();
+	}
+
+	async loadSettings() {
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	}
+
+	async saveSettings() {
+		await this.saveData(this.settings);
+	}
+}
