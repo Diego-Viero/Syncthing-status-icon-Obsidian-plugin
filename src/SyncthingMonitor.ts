9b35e8a19ac5f2364edd46243a39257f9b42f98e
Diego-Viero <diego.viero@protonmail.com>
2025-05-07 13:39:26 +0200

    rename file to match class name

--- src/SyncthingMonitor.ts
+++ src/SyncthingMonitor.ts
@@ -1,16 +1,38 @@
 import * as http from 'http';
 import { EventEmitter } from 'events';
 import { eventProcessor } from './eventsProcessor';
+import { Icon } from 'types/iconEnum';
+import { SyncthingStatusSettings } from 'types/settings';
 
 export class SyncthingMonitor extends EventEmitter {
   private token: string;
   private timeout: number
   private lastEventId: number | undefined;
+  private fileCompletion: number | undefined; 
+  private status: string;
 
-  public listenForEvents(token: string, timeout: number) {
-    this.token = token;
-    this.timeout = timeout;
+  public folderId: string | null;
+
+  public setStatusIcon: (_: Icon) => void;
+
+  public listenForEvents(
+    settings: SyncthingStatusSettings,
+    setStatusIcon: (_: Icon) => void
+  ) {
+    this.token = settings.syncthingToken;
+    this.timeout = settings.pollingTimeout;
+    this.folderId = settings.folderId || null;  // sets value to null if it's an empty string
+    this.status = "idle";
+    this.setStatusIcon = setStatusIcon;
+
     this.poll(); 
+  }
+
+  public setCompletion = (x: number): void => {
+    this.fileCompletion = x;
+  }
+  public setStatus = (s: string): void => {
+    this.status = s;
   }
 
   private poll() {
@@ -36,18 +58,15 @@ export class SyncthingMonitor extends EventEmitter {
 
       res.on('end', () => {
         try {
-
           const events = JSON.parse(body);
           const eventsLength = events.length;
 
           if (Array.isArray(events)) {
             for (const event of events) {
               this.lastEventId = Math.max(this.lastEventId ?? 0, event.id);
-              eventProcessor(event);
+              eventProcessor(event, this);
             }
           }
-
-          console.log(this.lastEventId);
         } catch (err) {
           console.error('Failed to parse Syncthing events:', err);
 
