967f61135920ac90be5e2240eab96fafd9c29820
Diego-Viero <diego.viero@protonmail.com>
2025-07-18 14:57:55 +0200

    Add settings tab and syncthing menu view

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import { SyncthingMonitor } from 'src/SyncthingMonitor';
-import { Plugin } from 'obsidian';
+import { Plugin, WorkspaceLeaf } from 'obsidian';
 import { SettingsTab } from './settings';
 import { Icon } from 'types/iconEnum';
 import { SyncthingStatusSettings } from 'types/settings';
 import { createWidget } from './widget';
+import { SyncthingMenuView, VIEW_TYPE_SYNCTHING } from './SyncthingMenuView';
 
 const DEFAULT_SETTINGS: SyncthingStatusSettings = {
-	pollingTimeout: 30,	// Seconds
+	pollingTimeout: 1,	// Seconds
 	syncthingToken: "",
 	folderId: ""
 }
@@ -18,10 +19,23 @@ export default class SyncthingStatus extends Plugin {
 	async onload() {
 
 		const monitor = new SyncthingMonitor();
+		console.log('test')
 
 		await this.loadSettings();
 
-    this.addSettingTab(new SettingsTab(this.app, this));
+		this.addSettingTab(new SettingsTab(this.app, this));
+
+
+		this.registerView(
+			VIEW_TYPE_SYNCTHING,
+			(leaf) => new SyncthingMenuView(leaf, monitor)
+		);
+
+		//TODO: add syncthing icon
+		this.addRibbonIcon('dice', 'Activate view', () => {
+			this.activateView();
+		});
+
 
 		this.statusBarItem = this.addStatusBarItem();
 		this.statusBarItem.setText('Loading...');
@@ -37,16 +51,47 @@ export default class SyncthingStatus extends Plugin {
 		});
 
 		monitor.listenForEvents(
-			this.settings, 
+			this.settings,
 			this.setStatusIcon
 		);
 
-		createWidget(this.statusBarItem);
+		this.addCommand({
+			id: 'display-syncthing-view',
+			name: 'Display Syncthing information',
+			callback: () => {
 
+				this.activateView();
+				//TODO open SyncthingView
+				//new ExampleModal(this.app, monitor).open();
+			},
+		});
+
+		createWidget(this.statusBarItem, monitor);
 	}
 
 	async loadSettings() {
 		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	}
+
+	async activateView() {
+		const { workspace } = this.app;
+
+		let leaf: WorkspaceLeaf | null = null;
+		const leaves = workspace.getLeavesOfType(VIEW_TYPE_SYNCTHING);
+
+		if (leaves.length > 0) {
+			// A leaf with the view already exists
+			leaf = leaves[0];
+		} else {
+			// View could not be found in the workspace, create a new leaf in the right sidebar
+			leaf = workspace.getRightLeaf(false);
+			await leaf?.setViewState({ type: VIEW_TYPE_SYNCTHING, active: true });
+		}
+
+		if (leaf)
+			workspace.revealLeaf(leaf);
+		else
+			console.error(`No leaf found: ${leaf}`)
 	}
 
 	async saveSettings() {
