0f87a2d732b5852a9ae29e00ffc73e9d31981d32
Diego-Viero <diego.viero@protonmail.com>
2025-07-19 18:32:46 +0200

    List number of available devices

--- src/SyncthingMenuView.ts
+++ src/SyncthingMenuView.ts
@@ -7,7 +7,8 @@ interface StatusState {
   status: string,
   fileCompletion: number,
   globalItems: number,
-  needItems: number
+  needItems: number,
+  availableDevices: number
 }
 
 export class SyncthingMenuView extends ItemView implements StatusState {
@@ -18,6 +19,7 @@ export class SyncthingMenuView extends ItemView implements StatusState {
   globalItems: number;
   needItems: number;
   connectedDevicesCount: number; 
+  availableDevices: number;
 
   constructor(leaf: WorkspaceLeaf, syncthingMonitor: SyncthingMonitor) {
     super(leaf);
@@ -27,6 +29,9 @@ export class SyncthingMenuView extends ItemView implements StatusState {
     this.globalItems = this.monitor.globalItems || NaN;
     this.needItems = this.monitor.needItems || NaN;
     this.connectedDevicesCount = this.monitor.connectedDevicesCount || NaN; // Initialize
+    this.availableDevices = this.monitor.availableDevices || NaN;
+
+    this.icon = 'syncthing-icon'
   }
 
   getViewType() {
@@ -49,24 +54,65 @@ export class SyncthingMenuView extends ItemView implements StatusState {
     this.monitor.off('status-update', this.handleStatusUpdate);
   }
 
-  private handleStatusUpdate = (data: StatusState & { connectedDevicesCount: number }) => {
+  private handleStatusUpdate = (data: StatusState & { connectedDevicesCount: number, availableDevices: number }) => {
     this.status = data.status;
     this.fileCompletion = data.fileCompletion;
     this.globalItems = data.globalItems;
     this.needItems = data.needItems;
     this.connectedDevicesCount = data.connectedDevicesCount;
+    this.availableDevices = data.availableDevices;
     this.render(); // Re-render the view with updated data
   }
 
   private render() {
     const container = this.containerEl.children[1];
     container.empty();
+    //container.addClass('syncthing-status-view');
+
     container.createEl('h4', { text: 'Syncthing Status' });
-    container.createEl('div', { text: `Status: ${this.status}`});
-    container.createEl('div', { text: `Files synced: ${parseFloat(this.fileCompletion.toFixed(2))}%`});
-    container.createEl('div', { text: `Total files: ${this.globalItems}`});
-    container.createEl('div', { text: `Files not synced: ${this.needItems}`});
-    container.createEl('div', { text: `Connected devices: ${this.connectedDevicesCount}`});
+
+    if (!this.monitor.isTokenSet) {
+      container.createEl('p', { text: 'Please insert your Syncthing API key in the plugin settings to monitor status.' });
+      return;
+    }
+
+    if (this.status === 'Invalid API key') {
+      container.createEl('p', { text: 'Invalid Syncthing API key. Please check your settings.' });
+      return;
+    }
+
+    // Status
+    /*
+    const statusItem = container.createEl('div', { cls: 'syncthing-status-item' });
+    statusItem.createEl('span', { cls: 'syncthing-status-label', text: 'Status:' });
+    const statusValue = statusItem.createEl('span', { cls: 'syncthing-status-value', text: this.status });
+    if (this.status === 'connected') {
+      statusValue.addClass('status-connected');
+    } else if (this.status === 'disconnected') {
+      statusValue.addClass('status-disconnected');
+    } else if (this.status === 'syncing') {
+      statusValue.addClass('status-syncing');
+    }
+    */
+
+    // File Completion
+    const fileCompletionItem = container.createEl('div', { cls: 'syncthing-status-item syncthing-no-border' }); // Added syncthing-no-border class
+    fileCompletionItem.createEl('span', { cls: 'syncthing-status-label', text: 'Sync status:' });
+    fileCompletionItem.createEl('span', { cls: 'syncthing-status-value', text: `${isNaN(this.fileCompletion) ? '0.00' : parseFloat(this.fileCompletion.toFixed(2))}%` });
+    const progressBarContainer = container.createEl('div', { cls: 'syncthing-progress-bar-container' });
+    const progressBar = progressBarContainer.createEl('div', { cls: 'syncthing-progress-bar' });
+    progressBar.style.width = `${isNaN(this.fileCompletion) ? 0 : this.fileCompletion}%`;
+
+    // Files not synced
+    const needItemsItem = container.createEl('div', { cls: 'syncthing-status-item' });
+    const needItemsLabel = needItemsItem.createEl('span', { cls: 'syncthing-status-label', text: 'Files not synced' });
+    needItemsLabel.createEl('span', { text: ' (includes .obsidian files)', cls: 'syncthing-small-text' });
+    needItemsItem.createEl('span', { cls: 'syncthing-status-value', text: `${isNaN(this.needItems) ? 0 : this.needItems}` });
+
+    // Connected devices
+    const connectedDevicesItem = container.createEl('div', { cls: 'syncthing-status-item' });
+    connectedDevicesItem.createEl('span', { cls: 'syncthing-status-label', text: 'Connected devices:' });
+    connectedDevicesItem.createEl('span', { cls: 'syncthing-status-value', text: `${isNaN(this.connectedDevicesCount) ? 0 : this.connectedDevicesCount}/${isNaN(this.availableDevices) ? 0 : this.availableDevices}` });
   }
 
   async setState(state: StatusState, result: ViewStateResult): Promise<void> {
@@ -83,6 +129,9 @@ export class SyncthingMenuView extends ItemView implements StatusState {
     }
     if (state.needItems) {
       this.needItems = state.needItems;
+    }
+    if (state.availableDevices) {
+      this.availableDevices = state.availableDevices;
     }
     // Note: connectedDevicesCount is updated via the custom event, not setState
     // The setState method is primarily for Obsidian's internal view state management.
